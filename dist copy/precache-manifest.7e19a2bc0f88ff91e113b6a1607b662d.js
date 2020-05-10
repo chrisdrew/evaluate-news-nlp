28533f1fa7e49882bcb6e7df4c758924496bad8c
chris <chris@republicbjj.com>
2020-05-10 09:41:58 -0600

    lesson 4 complete

---
+++ dist copy/precache-manifest.7e19a2bc0f88ff91e113b6a1607b662d.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfa7894f4c55d02f993ac6540261940",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf48e124a1af235f5649",
+    "url": "main.css"
+  },
+  {
+    "revision": "bf48e124a1af235f5649",
+    "url": "main.js"
+  }
+]);
